5ca5f57a7da8eb9241b4c77a5261180bca30240a
ssebide <sebiddejohn136@gmail.com>
2025-06-21 09:57:47 +0300

    Make the OS test compatible

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 #![no_std] //dont link Rust standard library
 #![no_main] //disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
 //static HELLO: &[u8] = b"Hello World!";
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[unsafe(no_mangle)] //dont mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -31,6 +42,9 @@ pub extern "C" fn _start() -> ! {
     // .unwrap();
 
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
     //panic!("Some panic message");
     loop {}
 }
@@ -40,4 +54,11 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    print!("[ok]");
 }
