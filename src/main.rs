383310309c2337563d9d968f7b19279e5b71cee9
ssebide <sebiddejohn136@gmail.com>
2025-06-21 23:35:45 +0300

    CPU Exceptions

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,15 @@ use ssebide_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    ssebide_os::init(); // new
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash ");
     loop {}
 }
 
