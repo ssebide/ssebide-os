bc04c80a51cb43030412b573d69a5a2e3118903e
ssebide <sebiddejohn136@gmail.com>
2025-06-21 20:41:39 +0300

    Testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,25 +7,18 @@
 use core::panic::PanicInfo;
 use ssebide_os::println;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     ssebide_os::test_panic_handler(info)
