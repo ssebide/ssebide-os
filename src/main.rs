69806e967ee11a134cfe01f2efec84b25769986d
ssebide <sebiddejohn136@gmail.com>
2025-06-23 13:14:04 +0300

    Double faults

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,20 @@ pub extern "C" fn _start() -> ! {
 
     ssebide_os::init(); // new
 
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
+
+    //trigger  a page fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // }
+
     // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    //x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
@@ -35,4 +47,9 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     ssebide_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
