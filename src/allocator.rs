e090677665490649f06f2ff4ed48e6d884ade812
ssebide <sebiddejohn136@gmail.com>
2025-06-24 21:32:04 +0300

    Heap allocator designs

--- src/allocator.rs
+++ src/allocator.rs
@@ -2,6 +2,19 @@ use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 
 pub struct Dummy;
+pub mod bump;
+pub mod fixed_size_block;
+pub mod linked_list;
+
+// use bump::BumpAllocator;
+
+// #[global_allocator]
+// static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
+use linked_list::LinkedListAllocator;
+
+#[global_allocator]
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
@@ -16,10 +29,10 @@ unsafe impl GlobalAlloc for Dummy {
     }
 }
 
-use linked_list_allocator::LockedHeap;
+// use linked_list_allocator::LockedHeap;
 
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+// #[global_allocator]
+// static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 use x86_64::{
     structures::paging::{
@@ -53,4 +66,25 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
